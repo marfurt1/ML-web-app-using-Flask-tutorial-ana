fee89f8db79d508e343d6b631014edce693c8b48
marfurta <anamariamarfurt@gmail.com>
2022-08-17 00:50:27 +0000

    ultimo antes de subir

--- src/test.py
+++ src/test.py
@@ -2,27 +2,26 @@ import pickle
 import pandas as pd
 import numpy as np
 
-'''
 ## Para resultado Yes
-country = 'Colombia'
+"""country = 'Colombia'
 variety = 'Caturra'
 aroma = 7.83
 aftertaste = 7.67
 acidity = 7.33
 body = 7.67
 balance = 7.67
-moisture = 0.11
-'''
+moisture = 0.11"""
+
 
 ## Para resultado No
-country = 'Other'
+"""country = 'Other'
 variety = 'Other'
 aroma = 7.25
 aftertaste = 6.83
 acidity = 7.17
 body = 7.00
 balance = 7.17
-moisture = 0.11
+moisture = 0.11"""
 
 # datos para probar el modelo
 cols = ['country_of_origin', 'variety', 'aroma', 'aftertaste', 'acidity', 'body', 'balance', 'moisture']
